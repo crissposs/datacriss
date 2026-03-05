d30e6d361ef2ffc6fb17b83a5f53543a226040ab
Cristian 🦭 <119689098+crissposs@users.noreply.github.com>
2026-03-05 13:00:26 -0500

    Add files via upload

---
+++ log/logcommand.js
@@ -0,0 +1,17 @@
+const chalk = require('chalk');
+const cx = require("consola");
+const magenta = chalk.magenta;
+const whiteBright = chalk.whiteBright;
+
+function logCommand(ctx) {
+    if (ctx === undefined) {
+        console.log('Se te olvido definir el ctx dentro de logCommand de alguna funcion');
+    } else if (ctx?.message?.text === undefined) {
+
+    } else {
+        cx.info(whiteBright(`Comando `) + magenta(ctx.message.text) + whiteBright(` ejecutado \n`));
+    }
+
+}
+
+module.exports = logCommand;
